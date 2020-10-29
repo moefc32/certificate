e6d4364cd1c72458e915e3f1f83146ab2d65dda1
Faizal Chan <moefc32@gmail.com>
2020-10-29 10:41:36 +0700

    update

---
+++ res/js/script.js
@@ -0,0 +1,10 @@
+window.onscroll = function () {
+  scrollIndicator()
+};
+
+function scrollIndicator() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("bar").style.width = scrolled + "%";
+}
