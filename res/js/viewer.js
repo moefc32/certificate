ce46e55ee1d0c0d55d9db0b830d19ff8ac734099
Faizal Chan <moefc32@gmail.com>
2020-11-04 22:47:00 +0700

    update

--- res/js/viewer.js
+++ res/js/viewer.js
@@ -9,30 +9,46 @@ function scrollIndicator() {
   document.getElementById("bar").style.width = scrolled + "%";
 }
 
+// If absolute URL from the remote server is provided, configure the CORS
+// header on that server.
 var url = "{{ site.url }}/lib/{{ page.slug }}.pdf }}";
-var pdfjsLib = window["pdfjs-dist/build/pdf"];
-pdfjsLib.GlobalWorkerOptions.workerSrc = "//mozilla.github.io/pdf.js/build/pdf.worker.js";
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+// Asynchronous download of PDF
 var loadingTask = pdfjsLib.getDocument(url);
-loadingTask.promise.then(function (pdf) {
+loadingTask.promise.then(function(pdf) {
+  console.log('PDF loaded');
+  
+  // Fetch the first page
   var pageNumber = 1;
-  pdf.getPage(pageNumber).then(function (page) {
+  pdf.getPage(pageNumber).then(function(page) {
+    console.log('Page loaded');
+    
     var scale = 1.5;
-    var viewport = page.getViewport({
-      scale: scale
-    });
-    var canvas = document.getElementById("viewer");
-    var context = canvas.getContext("2d");
+    var viewport = page.getViewport({scale: scale});
+
+    // Prepare canvas using PDF page dimensions
+    var canvas = document.getElementById('the-canvas');
+    var context = canvas.getContext('2d');
     canvas.height = viewport.height;
     canvas.width = viewport.width;
+
+    // Render PDF page into canvas context
     var renderContext = {
       canvasContext: context,
       viewport: viewport
     };
     var renderTask = page.render(renderContext);
     renderTask.promise.then(function () {
-      console.log("Page rendered");
+      console.log('Page rendered');
     });
   });
 }, function (reason) {
+  // PDF loading error
   console.error(reason);
 });
