1eac4a7ac2d4306c6e28a0c4ad0f81b33e6f7036
Faizal Chan <moefc32@gmail.com>
2021-01-15 19:56:28 +0700

    update

--- res/js/script.js
+++ res/js/script.js
@@ -3,8 +3,8 @@ window.onscroll = function () {
 };
 
 function scrollIndicator() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
   document.getElementById("bar").style.width = scrolled + "%";
 }
